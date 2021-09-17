4d20b98d53c2f5014b7fc442406955aa6322a9ff
hiroshi-kato <hiroshikato1208@gmail.com>
2021-09-17 23:16:16 +0900

    tailwindcssを導入

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
