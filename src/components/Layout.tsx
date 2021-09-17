762f63a826ccb599332187aa62667a733a3f7f09
hiroshi-kato <hiroshikato1208@gmail.com>
2021-09-17 23:16:32 +0900

    mdxまわりを整理

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,9 @@
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      {/* <div className="max-w-prose mx-auto px-4"> */}
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <main style={{ maxWidth: '600px', padding: '16px' }}>{children}</main>
+      <header></header>
+      <div>
+        <main>{children}</main>
       </div>
     </>
   );
